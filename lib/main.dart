f56d15fa6d1cd4a81279016c516f60afc0a6c7de
Vashist-Agarwalla <vashist1202@gmail.com>
2021-07-20 21:37:48 +0530

    ✨ Add Scaffolding

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+              'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+            ),
+          ),
+        ),
       ),
     ),
   );
