06a196438cfd472490dd4a020e7583ca9e756ef6
Vashist-Agarwalla <vashist1202@gmail.com>
2021-07-21 00:30:31 +0530

    ⚡️ Add Image as Asset

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(
@@ -12,9 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-              'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-            ),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
